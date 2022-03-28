ec1faa6ae1a7a657acbdd56605995432915068e4
samriddha <samriddhabasu67@gmail.com>
2022-03-29 02:37:45 +0530

    Creating a 2 screen login page using a flutter app.

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:login_page/login.dart';
+import 'package:login_page/register.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyLogin(),
+    routes: {
+      'register': (context) => MyRegister(),
+      'login': (context) => MyLogin(),
+    },
+  ));
+}
